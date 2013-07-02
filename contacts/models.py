ac9b7a7d709dafff4e1b3f4fffeec5eb71b2d4a7
Mike <mike@Mikes-Mac-mini.local>
2013-07-03 00:57:44 +0800

    update

--- contacts/models.py
+++ contacts/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 class Contact(models.Model):
 	first_name = models.CharField(max_length=255)
+	user = models.CharField(max_length=255)
 	def __str__(self):
 		return self.first_name
