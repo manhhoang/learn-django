4ba31cb8fe83b822684f4e49d1517be69b39ffcb
Ha Manh Hoang <hamanhhoang1102@gmail.com>
2013-07-03 10:50:37 +0800

    update

--- contacts/models.py
+++ contacts/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Contact(models.Model):
-	first_name = models.CharField(max_length=255)
-	def __str__(self):
-		return self.first_name
+    user = models.ForeignKey(User)
+    first_name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.first_name
+		
+    def save(self, *args, **kwargs):
+	    return super(Contact, self).save(*args, **kwargs)
